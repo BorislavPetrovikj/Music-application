fe7167c8d8decd8eaa15a3473d21cf460e7f4f8c
Borko <petrovicborko392@gmail.com>
2021-01-23 11:29:04 +0100

    Routing

--- src/Components/Albums.js
+++ src/Components/Albums.js
@@ -3,15 +3,7 @@ import React from "react";
 const Albums = (props) => {
   return (
     <div className="Albums ">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <img src={`/images/albums/${props.albums}.jpg`} alt="img"></img>
-            <img src={`/images/albums/${props.albums1}.jpg`} alt="img"></img>
-            <img src={`/images/albums/${props.albums2}.jpg`} alt="img"></img>
-          </div>
-        </div>
-      </div>
+      <img src={`/images/albums/${props.cover}.jpg`} alt="img"></img>
     </div>
   );
 };
--- src/Components/ArtistsPage.js
+++ src/Components/ArtistsPage.js
@@ -21,11 +21,15 @@ const ArtistsPage = (props) => {
               alt="img"
             />
             <p>{currentEl.bio}</p>
-            <Albums
-              albums={currentEl.albums[0].cover}
-              albums1={currentEl.albums[1].cover}
-              albums2={currentEl.albums[2].cover}
-            />
+            <div className="row px-5 py-4">
+              {currentEl.albums.map((Element) => {
+                return (
+                  <div className="col-md-6 p-0">
+                    <Albums cover={Element.cover} />
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
@@ -34,15 +38,3 @@ const ArtistsPage = (props) => {
 };
 
 export default ArtistsPage;
-
-
-<div className="ArtistsPage text-center bgGray">
-            <img key={currentEl.cover} src={require(`../../images/covers/${currentEl.cover}.jpg`).default} alt="img" className="w-25 py-4" />
-            <h2 className="nameArtistsPage pb-3" key={currentEl.name}>{currentEl.name}</h2>
-            <p className="mb-0" key={currentEl.bio}>{currentEl.bio}</p>
-            <div className="row px-5 py-4">
-                {currentEl.albums.map(elem => {
-                    return (
-                        <div className="col-md-6 p-0" >
-                            <Albums cover={elem.cover} />
-                        </div>
